b3b4c82f6d4fd06f3700727e3d76135320e79c0f
Md-Ruhul-Amin-Rony <adiar1234@yahoo.com>
2025-05-15 16:24:54 +0200

    ready for deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/BlogPostReactAI/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
